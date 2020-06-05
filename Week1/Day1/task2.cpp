d26b7ab4e65972889855f0783cd1e47560c6e4b6
ojasvik <khuranaojasvi@gmail.com>
2020-06-05 06:46:34 -0700

    Day5

--- Week1/Day1/task2.cpp
+++ Week1/Day1/task2.cpp
@@ -1,3 +1,9 @@
+/*Vaibhav likes to play with numbers and he has n numbers. One day he was placing the numbers on the playing board just to 
+count that how many numbers he have. He was placing the numbers in increasing order i.e. from 1 to n. But when he was putting 
+the numbers back into his bag, some numbers fell down onto the floor. He picked up all the numbers but one number, he couldn't 
+find. Now he have to go somewhere urgently, so he asks you to find the missing number.*/
+
+
 #include <iostream>
 using namespace std;
 
