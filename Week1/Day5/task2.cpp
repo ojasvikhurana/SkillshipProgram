3bb6c8afb4b27ec9131c337e972889d468087d2b
ojasvik <khuranaojasvi@gmail.com>
2020-06-07 04:31:14 -0700

    Updated

--- Week1/Day5/task2.cpp
+++ Week1/Day5/task2.cpp
@@ -1,3 +1,24 @@
 /* Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+Note that it's the kth smallest element in the sorted order, not the kth distinct element.*/
 
-Note that it is the kth smallest element in the sorted order, not the kth distinct element.*/
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        vector<int>ar(matrix.size(),0);
+        int count=0;
+        int mind=-1,mina=INT_MAX;
+        for(int i=0;count<k;i++){
+            mind=-1,mina=INT_MAX;
+            for(int j=0;j<matrix.size();j++){
+                if(ar[j]==matrix[j].size())continue;
+                if(matrix[j][ar[j]]<mina){
+                    mina=matrix[j][ar[j]];
+                    mind=j;
+                }
+            }
+            count++;
+            ar[mind]++;
+        }
+        return matrix[mind][ar[mind]-1];
+    }
+};
