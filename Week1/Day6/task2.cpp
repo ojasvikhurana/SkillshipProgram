de9978bc2723589be5b4a07f05b93fb81d9ad09a
ojasvik <khuranaojasvi@gmail.com>
2020-06-07 04:19:25 -0700

    Day7

--- Week1/Day6/task2.cpp
+++ Week1/Day6/task2.cpp
@@ -13,3 +13,55 @@ q=3
 2 2
 1 2
 Ans : {4,4,7,9,0} */
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,q,ar[100000];
+    cin>>n;
+    for(int i=0;i<n;i++)cin>>ar[i];
+    cin>>q;
+    int frot=0;
+    for(int i=0;i<q;i++){
+        int dir,am;
+        cin>>dir>>am;
+        if(dir==1)frot-=am;
+        else frot+=am;
+    }
+    if(frot>0){
+        frot%=n;
+        int count=1;
+        for(int i=0;count<=n;i++){
+            int curr=i;
+            int prev=ar[i];
+            do{
+                int next=(curr+frot)%n;
+                int temp=ar[next];
+                ar[next]=prev;
+                prev=temp;
+                curr=next;
+                count++;
+            }while(curr!=i);
+        }
+    }else if(frot<0){
+        frot=abs(frot);
+        frot%=n;
+        int count=1;
+        for(int i=0;count<=n;i++){
+            int curr=i;
+            int prev=ar[i];
+            do{
+                int next=(curr-frot+n)%n;
+                int temp=ar[next];
+                ar[next]=prev;
+                prev=temp;
+                curr=next;
+                count++;
+            }while(curr!=i);
+        }
+    }
+    for(int i=0;i<n;i++)cout<<ar[i]<<" ";
+}
--- Week1/Day6/task3.cpp
+++ Week1/Day6/task3.cpp
@@ -7,3 +7,43 @@ You are given a target value to search. If found in the array return its index, 
 You may assume no duplicate exists in the array.
 
 Your algorithm's runtime complexity must be in the order of O(log n).*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==0)return -1;
+        int r=nums.size()-1,l=0;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            cout<<mid;
+            if(nums[mid]>nums[r])
+                l=mid+1;
+            else
+                r=mid;
+        }
+     //cout<<l;
+    
+        int beg=l;
+        l=0,r=nums.size()-1;
+        int s=0,e=nums.size()-1;
+        if(target>=nums[beg] and target<=nums[r]){
+            s=beg;
+        }else{
+            e=beg-1;
+        } 
+        
+   //     cout<<beg;
+     //   cout<<s<<e;
+       
+        while(s<=e){
+            int mid=e-((e-s)/2);
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                s=mid+1;
+            else
+                e=mid-1;
+        }
+        return -1;
+    }
+};
